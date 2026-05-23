b087563b419c391d9ea8a29dbab4886ff25c851b
Gio Kupatadze <38761156+Grouvya@users.noreply.github.com>
2026-05-23 18:55:31 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -324,9 +324,6 @@ ConnectionPadding 1
 ReducedConnectionPadding 0
 CircuitBuildTimeout 10
 CookieAuthentication 1
-UseBridges 1
-ClientTransportPlugin snowflake exec /usr/bin/snowflake-client -url https://snowflake-broker.torproject.net.global.prod.fastly.net/ -front cdn.sstatic.net -ice stun:stun.l.google.com:19302,stun:stun.voip.blackberry.com:3478,stun:stun.altar.com.pl:3478,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.sonetel.net:3478,stun:stun.stunprotocol.org:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478
-Bridge snowflake 192.0.2.3:1 2B280B23E1107BB62ABFC40DDCC8824814F80A72
 # --- END ANONSHIELD CONFIGURATION ---
 EOF
     echo -e "  ${GREEN}✓${NC} Configured transparent port, DNS, and control ports in /etc/tor/torrc"
